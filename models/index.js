0a6249ca3bb46adbcaee2bb9cd8dbb31eee85450
jkrouse13 <jon.krouse@gmail.com>
2017-07-13 21:35:41 -0400

    JFC

--- models/index.js
+++ models/index.js
@@ -10,14 +10,14 @@ var db        = {};
 
 if (process.env.DATABASE_URL) {
   // the application is executed on Heroku ... use the postgres database
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
+  Sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect:  'postgres',
     protocol: 'postgres',
     logging:  true //false
   });
 } else {
   // the application is executed on the local machine
-  sequelize = new Sequelize("postgres:///my_db");
+  Sequelize = new Sequelize('postgres:///my_db');
 }
 
 fs
